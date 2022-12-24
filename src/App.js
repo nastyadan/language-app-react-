6408650e84828b618c88763671d6ec5113646e8f
Anastasia Danilova <anastasia.proekt@gmail.com>
2022-12-25 00:51:26 +0300

    добавление таблицы

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 
-import './App.scss';
+
 import Card from './assets/components/card';
 import Footer from './assets/components/footer';
 import Header from './assets/components/header';
+import Table from './assets/components/table';
 import { listOfWords } from './assets/data/wordsData';
+import './App.scss';
+
 
  
 
 
 function App() {
+  
   return (
     <div className='App'>
      <Header ></Header>
      <section className="section-cards">
      {listOfWords.map((listOfWords) => 
      <Card english={listOfWords.english} transcription = {listOfWords.transcription} russian= {listOfWords.russian} tags ={listOfWords.tags}></Card> )}
+      </section>
+      <section className="section-listOfWords">
+  <Table></Table>
       </section>
      <Footer className="App"></Footer>
     </div>
---
+++ src/assets/components/table.jsx
@@ -0,0 +1,40 @@
+//компонент карточки (слово+перевод+транскрипция + возможность добавить слово в словарь?)
+
+import React from 'react';
+import tableStyle from '../styles/table.module.scss';
+import { listOfWords } from '../data/wordsData';
+
+
+
+
+
+class Table extends React.Component {
+
+    render() {
+        return (
+            <table className={tableStyle.table}>
+                <thead>
+                    <tr>
+                        <td className={tableStyle.tdName}>English</td>
+                        <td className={tableStyle.tdName}>Транскрипция</td>
+                        <td className={tableStyle.tdName}>Перевод</td>
+                        <td className={tableStyle.tdName}>Действие</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listOfWords.map(function (item) {
+                        return <tr key={item.id}>
+                            <td className={tableStyle.tdWords}>{item.english}</td>
+                            <td className={tableStyle.tdWords}>{item.transcription}</td>
+                            <td className={tableStyle.tdWords}>{item.russian}</td>
+                            <td className={tableStyle.tdWords}>  <button className={tableStyle.tableButton} >Сохранить</button></td>
+                        </tr>;
+                    })}
+
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default Table
